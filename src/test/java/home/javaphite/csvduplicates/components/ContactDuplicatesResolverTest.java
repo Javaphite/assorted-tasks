759d656be677d259076fc85ce6788aa2047268f3
Alexander Berezovchuk <javaphite@gmail.com>
2020-06-15 17:39:17 +0300

    Add new corner case coverage and algorithm update - if two different groups are merged through one element by different key fields.

--- src/test/java/home/javaphite/csvduplicates/components/ContactDuplicatesResolverTest.java
+++ src/test/java/home/javaphite/csvduplicates/components/ContactDuplicatesResolverTest.java
@@ -5,16 +5,16 @@ import org.junit.jupiter.api.Test;
 
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ContactDuplicatesResolverTest {
 
     @Test
     void shouldReturn111() {
         Map<String, List<String>> inputTable = new HashMap<>();
         inputTable.put("id", List.of("1", "2", "3"));
-        List<String> initialParentIds =  new ArrayList<>(3);
-        initialParentIds.add("null"); initialParentIds.add("null"); initialParentIds.add("null");
+        List<String> initialParentIds = new ArrayList<>(3);
+        initialParentIds.add("null");
+        initialParentIds.add("null");
+        initialParentIds.add("null");
         inputTable.put("parentId", initialParentIds);
         inputTable.put("email", List.of("E1", "E1", "E3"));
         inputTable.put("card", List.of("C1", "C3", "C3"));
@@ -31,8 +31,11 @@ class ContactDuplicatesResolverTest {
     void shouldReturn1111() {
         Map<String, List<String>> inputTable = new HashMap<>();
         inputTable.put("id", List.of("1", "2", "3", "4"));
-        List<String> initialParentIds =  new ArrayList<>(4);
-        initialParentIds.add("null"); initialParentIds.add("null"); initialParentIds.add("null"); initialParentIds.add("null");
+        List<String> initialParentIds = new ArrayList<>(4);
+        initialParentIds.add("null");
+        initialParentIds.add("null");
+        initialParentIds.add("null");
+        initialParentIds.add("null");
         inputTable.put("parentId", initialParentIds);
         inputTable.put("email", List.of("E1", "E1", "E3", "E4"));
         inputTable.put("card", List.of("C1", "C3", "C3", "C4"));
@@ -49,8 +52,11 @@ class ContactDuplicatesResolverTest {
     void shouldReturn1133() {
         Map<String, List<String>> inputTable = new HashMap<>();
         inputTable.put("id", List.of("1", "2", "3", "4"));
-        List<String> initialParentIds =  new ArrayList<>(4);
-        initialParentIds.add("null"); initialParentIds.add("null"); initialParentIds.add("null"); initialParentIds.add("null");
+        List<String> initialParentIds = new ArrayList<>(4);
+        initialParentIds.add("null");
+        initialParentIds.add("null");
+        initialParentIds.add("null");
+        initialParentIds.add("null");
         inputTable.put("parentId", initialParentIds);
         inputTable.put("email", List.of("E1", "E2", "E3", "E4"));
         inputTable.put("card", List.of("C1", "C2", "C3", "C4"));
@@ -79,11 +85,32 @@ class ContactDuplicatesResolverTest {
         Assertions.assertIterableEquals(List.of("1", "1", "1", "1", "1"), outputTable.get("parentId"));
     }
 
+    @Test
+    void shouldReturn11311() {
+        Map<String, List<String>> inputTable = new HashMap<>();
+        inputTable.put("id", List.of("1", "2", "3", "4", "5"));
+        inputTable.put("parentId", prepareInitialParentIds(5));
+        inputTable.put("email", List.of("E1", "E2", "E3", "E1", "E5"));
+        inputTable.put("card", List.of("C1", "C2", "C3", "C2", "C5"));
+        inputTable.put("phone", List.of("P1", "P2", "P3", "P4", "P2"));
+
+        Map<String, List<String>> outputTable =
+                new ContactDuplicatesResolver(inputTable, "id", "parentId").getResolvedTable();
+
+        System.out.println(outputTable);
+        Assertions.assertIterableEquals(List.of("1", "1", "3", "1", "1"), outputTable.get("parentId"));
+    }
+
     static private List<String> prepareInitialParentIds(final int numberOfRows) {
-        List<String> initialParentIds =  new ArrayList<>(numberOfRows);
-        for (int i=0; i < numberOfRows; i++) {
+        List<String> initialParentIds = new ArrayList<>(numberOfRows);
+        for (int i = 0; i < numberOfRows; i++) {
             initialParentIds.add("null");
         }
         return initialParentIds;
+    }
+
+    @Test
+    void catchDiscussion() {
+        System.out.println(Integer.valueOf(-100500).hashCode());
     }
 }
